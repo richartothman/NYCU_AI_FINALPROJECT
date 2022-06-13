dc97e69151302f629e8a84aa64cccc4ea5ba7fa1
Zryxion <owenking2710@gmail.com>
2022-06-13 16:56:06 +0800

    Search

---
+++ Pool/Search_AI.py
@@ -0,0 +1,47 @@
+from copy import deepcopy
+import numpy
+import pygame
+import Pool
+import time
+from math import pi,radians
+
+def Generate_Bestshots(game):
+    shot = None
+    MaxDist = 0
+    prevpos = game.getAllballPos()
+    for i in range(360):
+        New = deepcopy(game)
+        New.ForcetoCue(radians(i),2)
+        New.full_update()
+        if New.GameOver: continue
+
+        curpos = New.getAllballPos()
+        # print(prevpos[1:])
+        # print(prevpos)
+        if prevpos[1:] == curpos[1:]:continue
+
+        CurrDist = New.getDistribution()
+        if MaxDist < CurrDist:
+            shot = i
+            MaxDist = CurrDist
+
+    return shot
+
+if __name__ == '__main__':
+    NewGame = Pool.Game()
+
+    while not NewGame.Iswin:
+        shot = Generate_Bestshots(NewGame)    
+
+        print(shot)
+        NewGame.ForcetoCue(radians(shot),2)
+        while not NewGame.isStopped():
+            NewGame.update()
+            display = pygame.display.set_mode((Pool.width,Pool.height))
+            try:
+                NewGame.draw(display)
+            except:
+                pass
+            pygame.display.flip()
+    
+    print("Win!")
